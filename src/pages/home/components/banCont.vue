da6f750d15dcaa8261344686ab02f069ed1660aa
xieqianqian <xieqq.bj@saohuobang.com>
2018-03-28 12:22:48 +0800

    ADD: video

--- src/pages/home/components/banCont.vue
+++ src/pages/home/components/banCont.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="bangker-bannerContent">
-        <div>
+        <div class="bannerContent-video">
+            <video muted="muted" preload="auto" loop="loop" ref="video">
+                <source src="/static/video/home/23827870.mp4" type="video/mp4">
+                <!-- <source src="https://ccm.ddcdn.com/cms/site/branding/videos/tahiti/video.mp4" type="video/mp4"> -->
+            </video>
+        </div>
+        <div class="bannerContent-text">
             <p class="p1">Bangker Travel</p>
             <p class="p2">在线旅行革命性创新</p>
             <p class="p3">基于区块链技术&中国最大的定制旅行生态</p>
@@ -12,6 +18,10 @@
 </template>
 <script>
     export default {
-
+        mounted() {
+            this.$refs.video.oncanplay = ()=>{
+                this.$refs.video.play()
+            }
+        }
     }
 </script>
